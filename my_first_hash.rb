9451b64804b17c52458358784ae0312ed9011edf
Samantha Wong <smiwong920@gmail.com>
2019-02-03 15:50:34 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,14 +26,7 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  shipping_manifest[musket]
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
+  shipping_manifest["musket"] = 2 
+  shipping_manifest["gun powder"] = 4
 
 end
