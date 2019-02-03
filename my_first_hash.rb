39694bf03df1528584486e285552d900cde74030
Samantha Wong <smiwong920@gmail.com>
2019-02-03 15:54:00 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -5,9 +5,9 @@ end
 
 def shipping_manifest
   the_manifest = {
-  "whale bone corset" => 5,
-  "porcelain vase" => 2,
-  "oil painting" => 3
+  "whale bone corsets" => 5,
+  "porcelain vases" => 2,
+  "oil paintings" => 3
 }
 end
 
